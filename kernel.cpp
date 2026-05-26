f4ae414ddde5b5adb083832df4cc9e006af4d0cc
localopensource <localdev14@gmail.com>
2026-05-26 16:10:05 +0300

    a simple minimal working version

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,13 @@
-void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+void printf(const char* str)
+{
+    unsigned short* VideoMemory= (unsigned short*)0xb8000;
+    for(int i=0; str[i]!='\0'; ++i){
+        //VideoMemory[i]=(VideoMemory[i] & 0xFF00) | str[i];
+        VideoMemory[i] = (0x0F << 8) | (unsigned char)str[i];
+    }
+}
+
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
 {
     printf("Hello World ------- http://www.algorithman.de");
     while(1);
